f5048343d9b85452b66c7cb71f23bd1417a39110
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-20 17:12:15 +0100

    Cleanup

--- modules/kubernetes-service-account/outputs.tf
+++ modules/kubernetes-service-account/outputs.tf
@@ -3,21 +3,10 @@ output "kubernetes_service_accounts" {
   value = {
     for k, v in kubernetes_service_account.k8s_service_accounts :
     k => {
-      name      = v.metadata[0].name
-      namespace = v.metadata[0].namespace
+      name        = v.metadata[0].name
+      namespace   = v.metadata[0].namespace
       annotations = v.metadata[0].annotations
-      labels    = v.metadata[0].labels
+      labels      = v.metadata[0].labels
     }
   }
 }
-
-output "kubernetes_namespaces" {
-  description = "Set of created Kubernetes namespaces"
-  value = {
-    for k, v in kubernetes_namespace.namespaces :
-    k => {
-      name = v.metadata[0].name
-    }
-  }
-}
-
