c6e4f64c2ae71aca0b6106667d1d6dc1b509e0f4
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-20 07:46:03 +0100

    Add WIF

--- modules/google-workload-identity-federation/main.tf
+++ modules/google-workload-identity-federation/main.tf
@@ -11,7 +11,7 @@ resource "google_iam_workload_identity_pool_provider" "k3s" {
 
   project                            = var.gcp_project_name
   workload_identity_pool_id          = google_iam_workload_identity_pool.simple[each.key].workload_identity_pool_id
-  workload_identity_pool_provider_id = "k3s"
+  workload_identity_pool_provider_id = each.key
 
   oidc {
     issuer_uri        = each.value.issuer_uri
