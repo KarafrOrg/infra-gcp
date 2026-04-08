9dd2ff568d34865e9d0708dca8f27d18a7039e7b
matustoth <55988328+karafra@users.noreply.github.com>
2026-04-08 23:04:50 +0200

    Cleanup

--- live/production/main.tf
+++ live/production/main.tf
@@ -6,11 +6,6 @@ module "infra-gcp" {
   pub_sub_topic_prefix         = var.pub_sub_topic_prefix
   k8s_clusters                 = var.k8s_clusters
   external_identity_pools      = var.external_identity_pools
-  gcp_audience                 = var.gcp_audience
-  gcp_identity_token           = var.gcp_identity_token
-  gcp_region                   = var.gcp_region
-  gcp_service_account_email    = var.gcp_service_account_email
-  gcp_zone                     = var.gcp_zone
   kube_client_ca_cert          = var.kube_client_ca_cert
   kube_client_cert_data        = var.kube_client_cert_data
   kube_client_key_data         = var.kube_client_key_data
--- modules/infra-gcp/variables.tf
+++ modules/infra-gcp/variables.tf
@@ -3,35 +3,6 @@ variable "gcp_project_name" {
   description = "GCP project name"
   type        = string
 }
-
-variable "gcp_region" {
-  description = "GCP region"
-  type        = string
-}
-
-variable "gcp_zone" {
-  description = "GCP zone"
-  type        = string
-}
-
-variable "gcp_identity_token" {
-  type        = string
-  ephemeral   = true
-  description = "JWT identity token"
-}
-
-variable "gcp_audience" {
-  type        = string
-  description = "The fully qualified GCP identity provider name, e.g. '//iam.googleapis.com/projects/123456789/locations/global/workloadIdentityPools/my-tfc-pool/providers/the-tfc-provider'. This is the same audience value as you've configured in the identity_token block. Google requires this audience value to be set in the service account file itself as well as the token claim."
-  sensitive   = true
-  ephemeral   = true
-}
-
-variable "gcp_service_account_email" {
-  type      = string
-  sensitive = true
-  ephemeral = true
-}
 # endregion
 
 # region Kubernetes provider variables
