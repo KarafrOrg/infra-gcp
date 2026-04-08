02cc8e9623ede46c0ca879b42b0a7ec97919ebd4
matustoth <55988328+karafra@users.noreply.github.com>
2026-04-08 21:54:18 +0200

    Fix audience

--- live/production/versions.tf
+++ live/production/versions.tf
@@ -31,10 +31,6 @@ provider "google" {
   project = var.gcp_project_name
   region  = var.gcp_region
   zone    = var.gcp_zone
-  external_credentials {
-    audience              = var.gcp_audience
-    service_account_email = var.gcp_service_account_email
-    identity_token        = var.gcp_identity_token
-  }
+  access_token = var.gcp_identity_token
 }
 
